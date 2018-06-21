5d0e9b53f67e6dae8045b6c48d2727d64e3cda1f
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-06-21 15:10:50 +0900

    first commit

---
+++ source/ast.cpp
@@ -0,0 +1,17 @@
+#include "lexer.hpp"
+#include "ast.hpp"
+
+static int CurTok;
+int getNextToken() {
+	return CurTok = gettok();
+}
+
+/// LogError* - These are little helper functions for error handling.
+std::unique_ptr<ExprAST> LogError(const char *Str) {
+	fprintf(stderr, "LogError: %s\n", Str);
+	return nullptr;
+}
+std::unique_ptr<PrototypeAST> LogErrorP(const char *Str) {
+	LogError(Str);
+	return nullptr;
+}
